041cb9a74ec814c71dae5287609312ea6c1ffff7
SomayaAhmed1430 <s.ramadan02185@dmu.edu.eg>
2024-12-21 03:33:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+using Suduko;
+
+int[,] sudokuMatrix = new int[9, 9]
+{
+    {5, 3, 0, 0, 7, 0, 0, 0, 0},
+    {6, 0, 0, 1, 9, 5, 0, 0, 0},
+    {0, 9, 8, 0, 0, 0, 0, 6, 0},
+    {8, 0, 0, 0, 6, 0, 0, 0, 3},
+    {4, 0, 0, 8, 0, 3, 0, 0, 1},
+    {7, 0, 0, 0, 2, 0, 0, 0, 6},
+    {0, 6, 0, 0, 0, 0, 2, 8, 0},
+    {0, 0, 0, 4, 1, 9, 0, 0, 5},
+    {0, 0, 0, 0, 8, 0, 0, 7, 9}
+};
+
+var suduko = new SudukoGame(sudokuMatrix);
+
+while (!suduko.IsComplete())
+{
+    Console.Clear();
+    suduko.PrintGrid();
+
+    Console.WriteLine("\nEnter row (1-9)");
+    int row = int.Parse(Console.ReadLine()) - 1;
+
+    Console.WriteLine("\nEnter column (1-9)");
+    int col = int.Parse(Console.ReadLine()) - 1;
+
+    Console.WriteLine("\nEnter number (1-9)");
+    int num = int.Parse(Console.ReadLine());
+
+    if (suduko.IsValidMove(row, col, num))
+    {
+        suduko.MakeMove(row, col, num);   
+    }
+    else
+    {
+        Console.WriteLine("Invalid move! Press any key to try again...");
+        Console.ReadKey();
+    }
+}
+
+Console.Clear();
+suduko.PrintGrid();
+Console.WriteLine("Congratulations! You've completed the Sudoku.");
---
+++ Suduko.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suduko
+{
+    internal class SudukoGame
+    {
+        private int[,] matrix;
+
+        public SudukoGame(int[,] matrix)
+        {
+            this.matrix = matrix;
+        }
+
+        public bool IsComplete()
+        {
+            for (int i = 0; i < 9; i ++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (matrix[i, j] == 0)
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void PrintGrid()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (i % 3 == 0)
+                    Console.WriteLine("-------------------------");
+
+                for (int j = 0; j < 9; j++)
+                {
+                    if (j % 3 == 0)
+                        Console.Write("| ");
+
+                    Console.Write(matrix[i, j] == 0 ? ". " : $"{matrix[i, j]} ");
+                }
+                Console.WriteLine("|");
+            }
+            Console.WriteLine("-------------------------");
+        }
+
+        public void MakeMove(int row, int col, int num)
+        {
+            matrix[row, col] = num;
+        }
+
+        public bool IsValidMove(int row, int col, int num)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (matrix[row, i] == num || matrix[i, col] == num)
+                    return false;
+            }
+
+            int startRow = row / 3 * 3;
+            int startCol = col / 3 * 3;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (matrix[startRow + i, startCol + j] == num)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
